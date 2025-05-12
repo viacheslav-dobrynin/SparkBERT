e21cf125c79a0e10731f16cf395917d82fb5b46f
Viacheslav Dobrynin <w.v.dobr@gmail.com>
2025-05-12 18:02:56 +0400

    refactor: remove commented code

--- src/vector_index.rs
+++ src/vector_index.rs
@@ -17,13 +17,5 @@ pub fn reconstruct_batch(
             let idx = labels[i];
             index.reconstruct(idx, chunk).map_err(anyhow::Error::from)
         })?;
-    //labels
-    //    .par_iter()
-    //    .map(|&idx| {
-    //        let mut emb = vec![0.0f32; d];
-    //        index.reconstruct(idx, &mut emb)?;
-    //        anyhow::Ok(emb)
-    //    })
-    //    .collect::<anyhow::Result<Vec<_>>>()
     anyhow::Ok(flat_embs)
 }
